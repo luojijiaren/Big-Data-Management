7628775ab2e0eaff49544c2eb793a3728d480d4e
Fangling Zhang <luojijiaren@163.com>
2017-02-11 15:41:09 -0500

    project 1

---
+++ Project 1/Q1_Customers.java
@@ -0,0 +1,30 @@
+public class Customers
+{
+	
+     public String randomWord() {
+		 int length = rand.nextInt(10) + 11;
+		 String word = "";
+		 for ((int i=0;i<length;i++) {
+			 word +=(char) randomChar();
+		 }
+		 return word;
+		 }
+
+     public static void main(String[] args){
+		
+                Dataset data= new DefaultDataset();
+                for (int i=0;i<10;i++) {
+                     Instance tmpInstance = InstanceTools.randomInstance(25);
+                     data.add(temInstance);
+                     }
+	           	System.out.println(data);
+
+                int ID = 1:50000;
+                char UserName = randomWord();
+                int Age = rand.nextInt(60)+10;
+                int CountryCOde=rand.nextInt(10);
+                float Salary=Math.random()*9900+100;
+                sb.append(all
+
+	}
+}
---
+++ Project 1/Q1_Transactions.java
@@ -0,0 +1,34 @@
+public class Transactions
+{
+    public String randomWord() {
+		 int length = rand.nextInt(30) + 21 ;
+		 String word = "";
+		 for ((int i=0;i<length;i++) {
+			 word +=(char) randomChar();
+		 }
+		 return word;
+	}
+
+	
+	
+	public static void main(String[] args)
+	{
+		
+                Dataset data= new DefaultDataset();
+                for (int i=0;i<10;i++) {
+                     Instance tmpInstance = InstanceTools.randomInstance(25);
+                     data.add(temInstance);
+                     }
+		        System.out.println(data);
+
+
+                int TransID = 1:5000000;
+                int CustID
+			    float TransTotal=Math.random()*990+10;
+                int TransNumItems = rand.nextInt(9)+1;
+                char TransDesc=randomWord();
+
+                sb.append(all
+
+	}
+}
