d09da7ae2082bc3b3341465ca5fecdc8f7c7071a
Fangling Zhang <luojijiaren@163.com>
2017-02-11 18:00:24 -0500

    Update Q1_Customers.java

--- Project 1/Q1_Customers.java
+++ Project 1/Q1_Customers.java
@@ -1,3 +1,10 @@
+import java.io.*
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+
+
 public class Customers
 {
 	
@@ -11,7 +18,19 @@ public class Customers
 		 }
 
      public static void main(String[] args){
-		
+		try {
+			String filename = "C:/Users/fzhan/Documents/GitHub/Big-Data-Management/Project 1/NewExcelFiles.xls";
+			HSSFWorkbook workbook = new HSSFWorkbook();
+			HSSFSheet sheet =workbook.createsheet("FirstSheet");
+			
+			HSSFRow rowhead=sheet.createRow((short)0);
+			rowhead.createCell(0).setCellValue(
+			rowhead.createCell(1).setCellValue(
+			rowhead.createCell(2).setCellValue(
+			rowhead.createCell(3).setCellValue(
+	     
+	     
+	     
                 Dataset data= new DefaultDataset();
                 for (int i=0;i<10;i++) {
                      Instance tmpInstance = InstanceTools.randomInstance(25);
