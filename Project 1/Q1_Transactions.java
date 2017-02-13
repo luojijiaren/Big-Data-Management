e475280a1850764c0815bc1bf3a577f2f4e5bdad
Fangling Zhang <luojijiaren@163.com>
2017-02-12 19:49:42 -0500

    p1

--- Project 1/Q1_Transactions.java
+++ Project 1/Q1_Transactions.java
@@ -1,34 +1,65 @@
-public class Transactions
-{
+import  java.io.*;
+import  org.apache.poi.xssf.usermodel.XSSFSheet;
+import  org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import  org.apache.poi.xssf.usermodel.XSSFRow;
+import  org.apache.poi.xssf.usermodel.XSSFCell;
+import java.util.Random;
+
+public class Q1_Transactions{
+
     public String randomWord() {
-		 int length = rand.nextInt(30) + 21 ;
-		 String word = "";
-		 for ((int i=0;i<length;i++) {
-			 word +=(char) randomChar();
-		 }
+		 Random randlen=new Random();
+		 int length = randlen.nextInt(20) + 31;
+		 char[] chars = "abcdefghijklmnopqrstuvwlyz".toCharArray();
+		 StringBuilder sb=new StringBuilder();
+		 for (int i=0;i<length;i++) {
+			 char c = chars[randlen.nextInt(chars.length)];
+			 sb.append(c);
+		     }
+		 String word = sb.toString();
 		 return word;
-	}
-
-	
-	
-	public static void main(String[] args)
-	{
-		
-                Dataset data= new DefaultDataset();
-                for (int i=0;i<10;i++) {
-                     Instance tmpInstance = InstanceTools.randomInstance(25);
-                     data.add(temInstance);
-                     }
-		        System.out.println(data);
+		 }
+		 
 
 
-                int TransID = 1:5000000;
-                int CustID
-			    float TransTotal=Math.random()*990+10;
-                int TransNumItems = rand.nextInt(9)+1;
-                char TransDesc=randomWord();
+    public static void main(String[]args) {
+        try {
+            String filename = "C:/Users/fzhan/Documents/GitHub/Big-Data-Management/Project 1/Transactions.xls" ;
+            XSSFWorkbook workbook = new XSSFWorkbook();
+			Random rand=new Random();
+			
+            XSSFSheet sheet = workbook.createSheet("FirstSheet");  
 
-                sb.append(all
+            XSSFRow rowhead = sheet.createRow((int)0);
+            rowhead.createCell(0).setCellValue("TransID");
+			rowhead.createCell(1).setCellValue("CustID");
+            rowhead.createCell(2).setCellValue("TransTotal");
+            rowhead.createCell(3).setCellValue("TransNumItems");
+			rowhead.createCell(4).setCellValue("TransDesc");
+			
+			Q1_Transactions test = new Q1_Transactions();
 
-	}
+            for (int i=1;i<=500000;i++){
+			    XSSFRow row = sheet.createRow((int)i);
+                row.createCell(0).setCellValue(i);
+			    int r1= rand.nextInt(49999)+1;
+                row.createCell(1).setCellValue(r1);                
+				float r2=rand.nextFloat()*990+10;
+                row.createCell(2).setCellValue(r2);
+			    int r3= rand.nextInt(9)+1;
+                row.createCell(3).setCellValue(r3);
+                String r4= test.randomWord();
+                row.createCell(4).setCellValue(r4);
+                }
+
+
+            FileOutputStream fileOut = new FileOutputStream(filename);
+            workbook.write(fileOut);
+            fileOut.close();
+            System.out.println("Your excel file has been generated!");
+
+        } catch ( Exception ex ) {
+            System.out.println(ex);
+        }
+    }
 }
---
+++ Project 1/Q3_1 .java
@@ -0,0 +1,59 @@
+package org.apache.hadoop.Q3_1;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class Q3_1 {
+
+  public static class TokenizerMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text>{
+    private Text custID = new Text();    
+    private Text outString = new Text();
+
+      
+    public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter
+                    ) throws IOException {
+
+      String line = value.toString();
+      String[] record = line.split(",");
+      Integer code= Integer.parseInt(record[3]);
+      
+
+      if (code>=2 && code<=6) {   
+        custID.set(record[0]);
+        outString.set(record[1]+","+record[2]+","+record[3]+","+record[4]);
+        output.collect(custID, outString);
+      }
+    }
+  }
+  
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+
+    Job job = new Job(conf, "Q3_1");
+    job.setJarByClass(Q3_1.class);
+    job.setMapperClass(TokenizerMapper.class);
+
+    job.setOutputKeyClass(Text.class);
+
+    job.setOutputValueClass(Text.class);
+
+    job.setInputFormat(TextInputFormat.class);
+    job.setoutputFormat(TextoutputFormat.class);
+
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+    JobClient.runJob(job);
+  }
+}
